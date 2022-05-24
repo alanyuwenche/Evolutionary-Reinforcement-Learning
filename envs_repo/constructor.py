60fcc9257ec2f2a3e4cc01bfb0fd4851d0d3fc3f
alanyuwenche <alanyuwenche@gmail.com>
2022-05-24 12:38:42 +0800

    Add files via upload

--- envs_repo/constructor.py
+++ envs_repo/constructor.py
@@ -1,33 +1,16 @@
 from envs_repo.gym_wrapper import GymWrapper
 
 class EnvConstructor:
-    """Wrapper around the Environment to expose a cleaner interface for RL
-
-        Parameters:
-            env_name (str): Env name
-
-
-    """
+    #daytradeEnv
     def __init__(self, env_name, frameskip):
-        """
-        A general Environment Constructor
-        """
         self.env_name = env_name
         self.frameskip = frameskip
 
-        #Dummy env to get some macros
-        dummy_env = self.make_env()
-        self.is_discrete = dummy_env.is_discrete
-        self.state_dim = dummy_env.state_dim
-        self.action_dim = dummy_env.action_dim
-
+        daytrade = self.make_env()
+        self.is_discrete = daytrade.is_discrete
+        self.state_dim = daytrade.state_dim
+        self.action_dim = daytrade.action_dim
 
     def make_env(self, **kwargs):
-        """
-        Generate and return an env object
-        """
         env = GymWrapper(self.env_name, self.frameskip)
-        return env
-
-
-
+        return env  
